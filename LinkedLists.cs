fde77d95019c1a70e27fe7881ad926dd4ab4de9c
serniczek97 <56827658+serniczek97@users.noreply.github.com>
2023-11-24 20:27:37 +0100

    Initial commit

---
+++ LinkedLists.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    public class OneWayList
+    {
+        private static Node head;
+        private static Node currentNode;
+        private class Node
+        {
+            public int data;
+            public Node next;
+        }
+        public void Print()
+        {
+            Node node = head;
+            while (node != null)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+        }
+        public void Push(int newData)
+        {
+            Node node = new Node();
+            node.data = newData;
+            node.next = null;
+            if (head == null)
+            {
+                head = node;
+                currentNode = node;
+            }
+            if (currentNode != null)
+            {
+                currentNode.next = node;
+                currentNode = node;
+            }   
+        }
+    }
+    
+ 
+}
+
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using LinkedLists;
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OneWayList a = new OneWayList();
+            a.Push(5);
+            a.Push(10);
+            a.Push(15);
+            a.Print();
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(5);
+            list.AddLast(10);
+            list.AddLast(15);
+        }
+    }
+}
